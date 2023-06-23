1549664f6cc7811bb0767e3148c4c6d2f40a38f5
alejandrodeleonc <45014064+alejandrodeleonc@users.noreply.github.com>
2023-06-23 15:50:26 -0400

    Initial Commit

---
+++ tvMazeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tvMazeApp
+//
+//  Created by Alejandro De León on 23/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tvMazeAppUITests/tvMazeAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  tvMazeAppUITestsLaunchTests.swift
+//  tvMazeAppUITests
+//
+//  Created by Alejandro De León on 23/6/23.
+//
+
+import XCTest
+
+final class tvMazeAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
